158f6731465b8bae1f92913f1b2f6d955a6cf50c
Omar <omar.garcia@morsa.com.mx>
2025-12-02 11:31:42 -0600

    DEfiniendo ACCIONES en el REDUCER

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,7 +1,10 @@
 import { Activity } from "../types";
 
 export type ActivityActions = {
-
+  type: 'save-activity',
+  payload: {
+    newActivity: Activity
+  }
 };
 
 type ActivityState = {
@@ -16,5 +19,13 @@ export const activityReducer = (
   state: ActivityState = initialState,
   action: ActivityActions
 ) => {
-
+  if (action.type === 'save-activity') {
+    // Este código maneja la lógica para actualizar el state
+    console.log('Desde el type de save-activity');
+    
+    // return {
+    //   ...state,
+    //   activites: [...state.activites, action.payload.newActivity]
+    // };
+  }
 };
