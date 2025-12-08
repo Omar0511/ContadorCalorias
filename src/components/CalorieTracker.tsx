4fff585b49576496f3fca40494145dd82b849333
Omar <omar.garcia@morsa.com.mx>
2025-12-08 16:49:32 -0600

    Creando un COMPONENTE para las CANTIDADES

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,29 +1,50 @@
 import { useMemo } from "react";
 import type { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
 
 type CalorieTrackerProps = {
   activities: Activity[];
 };
 
 export default function CalorieTracker({ activities }: CalorieTrackerProps) {
-  const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities]);
-  const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities]);
-  
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
   return (
     <>
-      <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de Calorías
+      </h2>
 
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange-50">{caloriesConsumed}</span>
-          Consumidas
-        </p>
+        <CalorieDisplay calories={caloriesConsumed} text="Consumidas" />
 
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange-50">{caloriesBurned}</span>
+        <CalorieDisplay calories={caloriesBurned} text="Ejercicio" />
+
+        {/* <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className="font-black text-6xl text-orange-50">
+            {caloriesBurned}
+          </span>
           Ejercicios
-        </p>
+        </p> */}
       </div>
     </>
   );
-};
+}
